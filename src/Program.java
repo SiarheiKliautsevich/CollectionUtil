b14ea4ae3f2d06a8710b303eedd8eb8e09fbe47e
Sergey Klevtsevich <ipklevtsevich@mail.ru>
2022-05-24 20:37:52 +0300

    Created a new method

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class Program
 {
     public static void main(String[] args)
     {
+        ArrayList <String> arrayList = new ArrayList<>();
+        arrayList.add("Текст один два");
+        arrayList.add("три текст четыре");
+        arrayList.add("пять шесть");
+
+        System.out.println(getElementsByWord(arrayList, "текст"));
+    }
+    public static ArrayList <String> getElementsByWord (ArrayList<String> arrayList, String word)
+    {
+
+       return (ArrayList<String>) arrayList.stream().filter(a -> a.toUpperCase().contains(word.toUpperCase())).collect(Collectors.toList());
 
     }
 }
